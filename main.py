af11129762232740dcd5b147b315a7f7fc6e25cf
viktoriussuwandi <viktorius.tori@gmail.com>
2023-05-19 12:16:20 +0000

    add selected option on game level dropdown-item

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_bootstrap    import Bootstrap
+from flask_bootstrap import Bootstrap
 from controller.game import Game
 
 game = Game()
@@ -9,7 +9,11 @@ Bootstrap(app)
 @app.route("/")
 def home() :
   difficult_options = game.difficult_options
-  return render_template("index.html", options = difficult_options)
+  return render_template(
+    "index.html", 
+    options = difficult_options, 
+    select_option = difficult_options[1]
+  )
 
 if __name__ == "__main__" :
   app.run(debug=True, host="0.0.0.0", port=2000)
