9e56e67607db663761ddd5142069a94b0dac680a
viktoriussuwandi <viktorius.tori@gmail.com>
2023-05-26 06:41:41 +0000

    update function for game turn

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,8 @@
 // ----------------------------------------------------------------------------
 // Game Data
 // ----------------------------------------------------------------------------
-let GAME_DATA
+let GAME_DATA   = {}
+let GAME_STATUS = 'end'
 
 function get_Flask_Data() {
     let deferredData = new jQuery.Deferred();
@@ -19,10 +20,21 @@ function get_Flask_Data() {
     return deferredData; // contains the passed data
 };
 
-function update_turn() {
-  let checkStart = GAME_DATA["game_start"]
-  let checkOver  = GAME_DATA["game_over"]
-  console.log(`Start : ${checkStart} ; Over : ${checkOver}`)
+function update_game() {
+
+  //1.Get game data & update game status
+  let start = GAME_DATA["game_start"]
+  let over  = GAME_DATA["game_over"]
+  GAME_STATUS = ( start === true && over === false ) ? 'start' : 'end';
+
+  //2.Update Player turn element
+  let player_turn_element = $('.game-turn .player-turn');
+  let text_turn_element   = $('.game-turn .text-turn');
+  if(GAME_STATUS === "start") {
+    player_turn_element.text(GAME_DATA["player_turn"]);
+    text_turn_element.text("Turn");
+  } else { text_turn_element.text("Start game or select player"); }
+
 }
 
 // ----------------------------------------------------------------------------
@@ -43,17 +55,17 @@ $(document).ready(function () {
     
     //3.Get game data, & Change html button value of dropdown as selected, 
     //  and make the html button disable
-    let get_game_data = get_Flask_Data();
+    let update_game_data = get_Flask_Data();
     let level_btn     = $(".game-level .level-btn")
     let item_btn      = $(this)
     
-    $.when( get_game_data  ).done( function( data ) {
+    $.when( update_game_data  ).done( function( data ) {
       GAME_DATA = data
-      console.log(`Level : ${GAME_DATA["game_level"]}`)
-      update_turn()
+      
       level_btn.text(item_btn.text());
       level_btn.val(item_btn.text());
       level_btn.addClass('disabled');
+      update_game()
     });
     
   });
@@ -79,18 +91,18 @@ $(document).ready(function () {
     request.send();
     
     //3.Get data, & disabled O button
-    let get_game_data = get_Flask_Data();
+    let update_game_data = get_Flask_Data();
     let btnX          = $(this)
     let btnO          = $(".btn-role-O")
     
-    $.when( get_game_data  ).done( function( data ) {
+    $.when( update_game_data  ).done( function( data ) {
       GAME_DATA = data
-      console.log(`Roles : ${JSON.stringify(GAME_DATA["game_roles"])}`)
-      update_turn()
+
       btnX.addClass('disabled-color');
       btnO.addClass('disabled');
+      update_game()
     });
-    
+
   });
 });
 
@@ -110,16 +122,16 @@ $(document).ready(function () {
     request.send();
     
     //3.Get data, & disabled X button
-    let get_game_data = get_Flask_Data();
+    let update_game_data = get_Flask_Data();
     let btnX          = $(".btn-role-X")
     let btnO          = $(this)
     
-    $.when( get_game_data  ).done( function( data ) {
+    $.when( update_game_data  ).done( function( data ) {
       GAME_DATA = data
-      console.log(`Roles : ${GAME_DATA["game_roles"]}`)
-      update_turn()
+      
       btnX.addClass('disabled');
       btnO.addClass('disabled-color');
+      update_game()   
     });
     
   });
@@ -143,15 +155,16 @@ $(document).ready(function () {
     
     //3.Get data, change html button value of square cell
     //  make the square cell disable
-    let get_game_data = get_Flask_Data();
+    let update_game_data = get_Flask_Data();
     let btn           = $(this)
     
-    $.when( get_game_data  ).done( function( data ) {
+    $.when( update_game_data  ).done( function( data ) {
       GAME_DATA = data
-      console.log(`Turn : ${GAME_DATA["player_turn"]}`)
-      update_turn()
       btn.addClass('disabled');
+      update_game()
     });
     
   });
 });
+
+update_game()
