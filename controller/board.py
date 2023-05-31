94c16f11f0c7416044e663af7bbd9e1bd9d1024e
viktoriussuwandi <viktorius.tori@gmail.com>
2023-05-31 08:08:44 +0000

    checking index of selected cells (odd or even)

--- controller/board.py
+++ controller/board.py
@@ -27,43 +27,51 @@ class Board:
 
       # Identify if any sum of combination [sum of row, sum of col] is match with any of sum_winner
 
-      find_winner_pair = [ 
-        [a,b,c] for [a,b,c] in cell_pairs if 
+      find_winner_pair = [
+        [a,b,c] for [a,b,c] in cell_pairs if
         [ sum( [a[0],b[0],c[0]] ), sum( [a[1],b[1],c[1]] ) ] in self.sum_winner
       ]
-      
+
       winner_pair = find_winner_pair[0] if (
         find_winner_pair is not None and 
         len(find_winner_pair) > 0
       ) else []
 
-      # if len(winner_pair) > 0:
+      if len(winner_pair) > 0:
         # Winner_found is True if :
-        # 1. index of all winner_pair cells are odd and 
+        # 1. index of all winner_pair cells are even and 
         #    [sum of winner_pair_row, sum of winner_pair_col] in winner_pair is [3,3]] 
-        # 2. index of all winner_pair cells are even and 
+        # 2. index of all winner_pair cells are odd and 
         #    (cells has identical row or has identical col)
         
         #a.Check if index of all winner_pair cells are odd or even -> diagonal is even
-      is_odd      = len([ self.all_cells.index(cell) % 2 == 1 for cell in winner_pair[0] if 
-                    self.all_cells.index(cell) % 2 == 1]) == 3
-      is_even     = len([ self.all_cells.index(cell) % 2 == 0 for cell in winner_pair[0] if
-                     self.all_cells.index(cell) % 2 == 0]) == 3
-      is_diagonal = is_even == True and is_odd == False
+        is_odd      = set( [ self.all_cells.index(cell) % 2 == 1 for cell in winner_pair if 
+                      self.all_cells.index(cell) % 2 == 1] )
         
-        #b.Check if [sum of cells_row, sum of cells_col] in winner_pair is [3,3]]
+        is_even     = set( [ self.all_cells.index(cell) % 2 == 0 for cell in winner_pair if
+                      self.all_cells.index(cell) % 2 == 0] )
         
+        is_diagonal = is_even == True and is_odd == False
+        
+        #b.Check if sum of cells_row, sum of cells_col] in winner_pair is [3,3]
+        is_twin = [
+          sum( [ cell[0] for cell in winner_pair ] ),
+          sum( [ cell[1] for cell in winner_pair ] )
+        ] == [3,3]
+
         #c.Check if (cells has identical row or cells has identical col)
-        
-      return f'''
-      Cell pair        : {cell_pairs}
-      Find winner pair : {find_winner_pair}
-      Winner pair      : {winner_pair}
-      '''
 
+        return f'''
+        Cell pair   : {cell_pairs}
+        Odd         : {is_odd},
+        Even        : {is_even},
+        Winner pair : {winner_pair}
+        Diagonal    : {is_diagonal}
+        Twin        : {is_twin}
+        '''
 
-#----------------------------------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
   
   def create_board(self):
     if self.total_square is not None:
