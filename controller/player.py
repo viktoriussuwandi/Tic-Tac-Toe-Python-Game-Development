5d1ce1a479209453d9577cb1062d226b8e79ac5f
viktoriussuwandi <viktorius.tori@gmail.com>
2023-06-02 15:57:12 +0000

    prepare 3 additional functions for different levels of comp

--- controller/player.py
+++ controller/player.py
@@ -4,6 +4,15 @@ class Player :
     self.role  = None
     self.cells_selected = []
     self.score = 0
-  
+
+  def easy_comp_cell(self) :
+    pass
+    
+  def medium_comp_cell(self) :
+    pass
+    
+  def impossible_comp_cell(self) :
+    pass
+    
   def __repr__(self) :
     return f'Role : {self.role}\nScore : {self.score}\n Cells : {self.cells_selected}'
